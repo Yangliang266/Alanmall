95dbbf92db03fd837afc5273466442aea8389c65
xiaomi <546493589@qq.com>
2020-10-17 00:37:51 +0800

    vuejs user/login/register/home

---
+++ alanmall-front/src/page/test/test.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <y-header></y-header>
+        <p>this is test</p>
+        <a href="" @click="test">test</a>
+        <y-footer></y-footer>
+    </div>
+</template>
+
+<script>
+import { getAllTest } from '../../api/index.js'
+import YHeader from '../../common/header'
+import YFooter from '../../common/footer'
+export default {
+   data () {
+       return {
+           // todo
+       }
+   },
+   methods: {
+        test() {
+           getAllTest().then(res => {
+               if (res.success) {
+                    this.$router.push({
+                        path: '/user'
+                    })
+               }
+           })
+       }
+    },
+    // mounted () {
+    //     this.test();
+    // },
+    components: {
+        YHeader,
+        YFooter
+    }
+}
+</script>
+
