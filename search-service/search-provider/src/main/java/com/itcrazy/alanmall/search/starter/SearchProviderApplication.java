3f93f075087a4f6e58d5a2d1ec4929ddb728d382
xiaomi <546493589@qq.com>
2020-11-11 21:30:26 +0800

    search-service

---
+++ search-service/search-provider/src/main/java/com/itcrazy/alanmall/search/starter/SearchProviderApplication.java
@@ -0,0 +1,13 @@
+package com.itcrazy.alanmall.search.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchProviderApplication.class, args);
+	}
+
+}
