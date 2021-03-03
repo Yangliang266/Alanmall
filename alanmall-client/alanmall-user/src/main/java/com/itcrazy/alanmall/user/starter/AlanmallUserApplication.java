8498927fc6faa7af0c5c406cdcb0862205062c59
xiaomi <546493589@qq.com>
2021-03-03 15:52:30 +0800

    工程整体模块分类整合

---
+++ alanmall-client/alanmall-user/src/main/java/com/itcrazy/alanmall/user/starter/AlanmallUserApplication.java
@@ -0,0 +1,16 @@
+package com.itcrazy.alanmall.user.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.itcrazy.alanmall.user"})
+public class AlanmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlanmallUserApplication.class, args);
+    }
+
+
+}
